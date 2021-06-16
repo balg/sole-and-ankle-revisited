c2bed2d81a9578d1f30e61b86b7dd9496c853417
balg <2700278+balg@users.noreply.github.com>
2021-06-16 12:29:03 +0200

    Exercise 5

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,11 +12,6 @@ import VisuallyHidden from "../VisuallyHidden";
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
 
-  // For our mobile hamburger menu, we'll want to use a button
-  // with an onClick handler, something like this:
-  //
-  // <button onClick={() => setShowMobileMenu(true)}>
-
   return (
     <header>
       <SuperHeader />
@@ -27,6 +22,7 @@ const Header = () => {
         <DesktopNav>
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
+          <NavLink href="/men">Men</NavLink>
           <NavLink href="/men">Men</NavLink>
           <NavLink href="/women">Women</NavLink>
           <NavLink href="/kids">Kids</NavLink>
@@ -41,7 +37,11 @@ const Header = () => {
             <Icon id="search" />
             <VisuallyHidden>Search</VisuallyHidden>
           </UnstyledButton>
-          <UnstyledButton>
+          <UnstyledButton
+            onClick={() => {
+              setShowMobileMenu(true);
+            }}
+          >
             <Icon id="menu" />
             <VisuallyHidden>Open menu</VisuallyHidden>
           </UnstyledButton>
@@ -61,8 +61,8 @@ const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  overflow: auto;
 
   @media (${QUERIES.tabletAndBelow}) {
     border-top: 4px solid ${COLORS.gray[900]};
@@ -79,7 +79,7 @@ const MainHeader = styled.div`
 
 const DesktopNav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(1rem, 9.2vw - 4.5rem, 3.5rem);
   margin: 0px 48px;
 
   @media (${QUERIES.tabletAndBelow}) {
